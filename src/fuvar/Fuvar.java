da05e72152b4695da9639d4da17442088b11f2a2
NadasyB <nadasy.bendeguz@gmail.com>
2021-02-12 12:14:49 +0100

    2. feladat

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,7 +1,24 @@
 package fuvar;
 
 public class Fuvar {
-    public static void main(String[] args) {
-        
-    }   
+    private int taxiId;
+    private String indulas;
+    private int idotartam;
+    private double tav;
+    private double viteldij;
+    private double borravalo;
+    private String fizMod;
+
+    public Fuvar(String sor) {
+        String[] s = sor.split(";");
+        this.taxiId = Integer.parseInt(s[0]);
+        this.indulas = s[1];
+        this.idotartam = Integer.parseInt(s[2]);
+        this.tav = Double.parseDouble(s[3]);
+        this.viteldij = Double.parseDouble(s[4]);
+        this.borravalo = Double.parseDouble(s[5]);
+        this.fizMod = s[6];
+    }
+    
+    
 }
